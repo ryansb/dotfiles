82bfc52f9525b104b5af661bf04784fdef4a4154
Ryan S. Brown <sb@ryansb.com>
2013-06-13 13:31:28 -0500

    Move vimrcs, tweak zprezto

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,17 @@
 #!/bin/zsh
 export ZDOTDIR=$HOME/.dotfiles/zsh
 git clone --recursive Documents/dotfiles .dotfiles
+echo "Getting zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+echo "Getting spf13"
+sh <(curl http://j.mp/spf13-vim3 -L)
+
+echo "making symlinks"
+rcs="tmux.conf tmux gitconfig zpreztorc zshrc zlogin vimrc.local vimrc.bundles.local"
+for rcfile in ${=rcs}; do
+	ln -s "$HOME/.dotfiles/$rcfile" "$HOME/.$rcfile" 
 done
