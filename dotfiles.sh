4a07ec9a7f78ee7195fb989ea108c91b40662877
Ryan Brown <ryansb@csh.rit.edu>
2013-07-05 14:01:03 -0500

    rearrange dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,16 @@
 #!/bin/zsh
-export ZDOTDIR=$HOME/.dotfiles/zsh
-git clone --recursive Documents/dotfiles .dotfiles
 echo "Getting zprezto"
+
+export ZDOTDIR=$HOME/.dotfiles/zsh
+
 setopt EXTENDED_GLOB
+
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+
+ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/.zlogin ~/.zlogin
 
 echo "Getting spf13"
 sh <(curl http://j.mp/spf13-vim3 -L)
@@ -15,6 +20,3 @@ rcs="tmux.conf tmux gitconfig vimrc.local vimrc.bundles.local"
 for rcfile in ${=rcs}; do
 	ln -s "$HOME/.dotfiles/$rcfile" "$HOME/.$rcfile" 
 done
-
-ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/zsh/.zlogin ~/.zlogin
