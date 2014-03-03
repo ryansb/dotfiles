516146f8cfa33d139e39a57d683f8decab9aaedc
Ryan S. Brown <sb@ryansb.com>
2014-03-03 11:36:04 -0600

    add shortcuts for http ports

--- zsh/extrafuncs/securitygroups.zsh
+++ zsh/extrafuncs/securitygroups.zsh
@@ -54,6 +54,26 @@ function sg-single-rm {
     aws ec2 revoke-security-group-ingress --group-name "${1}" --port "${2}" --protocol tcp --cidr "${IPTOADD}"
 }
 
+function sg-http {
+    if (( ${#argv} < 1 )) ; then
+        echo 'usage: sg-http MYGROUP [1.2.3.4]' >&2
+        echo 'if IP not provided automatically uses your public IP' >&2
+        return 1
+    fi
+    sg-single "${1}" 80 "${2}"
+    sg-single "${1}" 443 "${2}"
+}
+
+function sg-http-rm {
+    if (( ${#argv} < 1 )) ; then
+        echo 'usage: sg-http-rm MYGROUP [1.2.3.4]' >&2
+        echo 'if IP not provided automatically uses your public IP' >&2
+        return 1
+    fi
+    sg-single-rm "${1}" 80 "${2}"
+    sg-single-rm "${1}" 443 "${2}"
+}
+
 function lssg {
     # if an argument is provided, it is passed as an egrep expression
     aws ec2 describe-security-groups --region us-east-1 | grep GroupName | sed -e 's/ *"GroupName": "\(.*\)".*/\1/' | sort -f | uniq | egrep --color=never -i -e "${1}"
