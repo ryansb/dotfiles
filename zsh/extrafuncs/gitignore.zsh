79aa22335c4e859b7c5bac0e93b5aaba424545a2
Ryan S. Brown <sb@ryansb.com>
2015-05-14 16:31:00 -0400

    Add workaround for F21 curl

--- zsh/extrafuncs/gitignore.zsh
+++ zsh/extrafuncs/gitignore.zsh
@@ -1,4 +1,4 @@
 # Usage: ignore python PyCharm vim > .gitignore
 function ignore() {
-    curl -s -L http://www.gitignore.io/api/${(j:,:)@}
+    curl -s -L --ciphers ecdhe_ecdsa_aes_128_gcm_sha_256 --insecure http://www.gitignore.io/api/${(j:,:)@}
 }
